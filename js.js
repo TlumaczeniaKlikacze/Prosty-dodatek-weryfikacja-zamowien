c4381d38c0321e305494f75c52854d41ffd05dc0
TlumaczeniaKlikacze <89144762+TlumaczeniaKlikacze@users.noreply.github.com>
2022-06-10 09:59:26 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+const interval =  setInterval(() => {
+    console.log('test')
+    if(document.getElementById('setVerifiedStatusToplayer') && document.getElementById('btn_status_change_no')){
+        document.getElementById('btn_status_change_no').click()
+        window.location.href = 'https://zooart.com.pl/panel/order-verification.php'
+    }
+}, 500);
